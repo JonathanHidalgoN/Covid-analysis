f8cf341c2532a6218798c55d5bb905adb0e3feff
Jonathan <joni7840850@gmail.com>
2023-04-12 15:43:16 -0600

    Remove irrelevant method

--- DataFormater.py
+++ DataFormater.py
@@ -1,7 +1,6 @@
 import csv
 from numpy import array as np_array
 from numpy import int32 as np_int32
-from numpy import delete as np_delete
 
 class DataFormater:
 
@@ -31,36 +30,19 @@ class DataFormater:
                     row[col] for row in reader
                     ])
         if num:
-            col = self._delete_str(col)
-        return col[items_to_delete:]
+            col = col[items_to_delete:].astype(np_int32)
+        else:
+            col = col[items_to_delete:]
+        return col
         
-    @staticmethod
-    def _delete_str(array : np_array) -> np_array:
-        """
-        This method deletes the string in the array.
-        Args:
-            array (array): The array to delete the string.
-        Returns:
-            array: The array without the string.
-        """
-        copy = array.copy()
-        idx = 0
-        while  idx < len(copy):  
-            try:
-                copy[idx] = np_int32(copy[idx])
-            except ValueError:
-                copy = np_delete(copy, idx)
-            idx += 1
-        return copy
-
 if __name__ == "__main__":
     path = "GtoDatOK150322 - GtoDatOK291020.csv"
     dataformat = DataFormater(path)
-    no = dataformat.read_col(0, False, 2)
-    date = dataformat.read_col(1, False, 2)
-    active_infected = dataformat.read_col(3, True, 1)
-    dead = dataformat.read_col(4, True, 1)
-    recovered = dataformat.read_col(5, True, 1)
-    vaccinated = dataformat.read_col(6, True, 1)
+    no = dataformat.read_col(0, False, 3)
+    date = dataformat.read_col(1, False, 3)
+    infected = dataformat.read_col(3, True, 2)
+    dead = dataformat.read_col(4, True, 2)
+    recovered = dataformat.read_col(5, True, 2)
+    vaccinated = dataformat.read_col(6, True, 2)
     #Missing data on vaccinated people (1)
-    assert len(no) == len(date) == len(active_infected) == len(dead) == len(recovered) == len(vaccinated)
+    assert len(no) == len(date) == len(infected) == len(dead) == len(recovered) == len(vaccinated)
