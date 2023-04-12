0802cdba77806ad431c2605e7760c89284557388
Jonathan <joni7840850@gmail.com>
2023-04-12 12:50:11 -0600

    Add method to compute infection rate of population

--- Rates.py
+++ Rates.py
@@ -1,6 +1,8 @@
 import Population 
 from itertools import product
 import typing
+import numpy as np
+from scipy.optimize import curve_fit
 
 class Rates:
 
@@ -12,6 +14,26 @@ class Rates:
 
     def __init__(self, population: Population):
         self._population = population
+        self._data_points = population.data_points
 
-    def compute_effective_infection_rate(self, days_to_consider: int = 151) -> typing.List[float]:
-        pass
+    def compute_effective_infection_rate(self, days_to_consider: int = 151) -> typing.Tuple[float, float]:
+        """
+        This method computes the effective infection rate.
+        Args:
+            days_to_consider (int): The number of days to consider.
+        Returns:
+            : The effective infection rate and error.
+        """
+        gamma_x = self._population.active_infected_people[self._population.data_points - days_to_consider:]
+        gamma_y = np.diff(
+            self._population.inmune_people[self._population.data_points - days_to_consider - 1:]
+        )
+        gamma, gamma_var = curve_fit(self.proportionality
+                                     , gamma_x, gamma_y)
+        gamma = gamma[0]
+        gamma_error = np.sqrt(gamma_var[0][0])
+        return gamma, gamma_error
+
+    @staticmethod
+    def proportionality(x,a):
+        return a*x
