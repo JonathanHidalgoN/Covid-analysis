e31d53aa936c9d0319d447edc0702fbfa5820794
Jonathan <joni7840850@gmail.com>
2023-04-13 14:59:43 -0600

    Add properties to class

--- Rates/Rate.py
+++ Rates/Rate.py
@@ -10,12 +10,95 @@ class Rate(abc.ABC):
     Attributes:
         xaxis (np.array): The x axis of the rate.
         yaxis (np.array): The y axis of the rate.
+        rate (float): The rate.
+        error (float): The error of the rate.
+        fit (Callable[[float]]): The fit of the rate.
+        confidence_interval (np.array): The confidence interval of the rate.
     """
 
     def __init__(self):
+        self._ready = False
+
+    def _check_setup(self):
+        """
+        This method checks if the rate is set up.
+        """
+        if not self._ready:
+            #I  dont like to return a string, but raise an exception is not a good idea
+            return "The rate is not set up. Please call the method set_up() before using the rate."
+
+    @property
+    def xaxis(self) -> np.array:
+        """
+        This method returns the x axis of the rate.
+        Returns:
+            np.array: The x axis of the rate.
+        """
+        self._check_setup()
+        return self._xaxis
+
+
+    @property
+    def yaxis(self) -> np.array:
+        """
+        This method returns the y axis of the rate.
+        Returns:
+            np.array: The y axis of the rate.
+        """
+        self._check_setup()
+        return self._yaxis
+
+    @property
+    def rate(self) -> float:
+        """
+        This method returns the rate.
+        Returns:
+            float: The rate.
+        """
+        self._check_setup()
+        return self._rate
+
+    @property
+    def error(self) -> float:
+        """
+        This method returns the error of the rate.
+        Returns:
+            float: The error of the rate.
+        """
+        self._check_setup()
+        return self._error
+
+    @property
+    def fit(self) -> Callable[[float]]:
+        """
+        This method returns the fit of the rate.
+        Returns:
+            Callable[[float]]: The fit of the rate.
+        """
+        self._check_setup()
+        return self._fit
+    
+    @property
+    def confidence_interval(self) -> np.array:
+        """
+        This method returns the confidence interval of the rate.
+        Returns:
+            float: The confidence interval of the rate.
+        """
+        self._check_setup()
+        return self._confidence_interval
+
+    def set_up(self):
+        """
+        This method sets up the rate.
+        """
         self._xaxis, self._yaxis = self._compute_axis()
         self._check_length()
-        self.data_points = len(self._xaxis)
+        self._data_points = len(self._xaxis)
+        self._rate, self._error = self._compute_rate_with_error()
+        self._fit = self._compute_fit()
+        self._confidence_interval = self._compute_confidence_interval()
+        self._ready = True
 
     def _check_length(self):
         """
@@ -24,9 +107,8 @@ class Rate(abc.ABC):
         if len(self._xaxis) != len(self._yaxis):
             raise ValueError("The x and y axis must have the same length.")
 
-    @property
     @abc.abstractmethod
-    def fit(self)-> Callable[[float]]:
+    def _compute_fit(self)-> Callable[[float]]:
         """
         This method returns the fit of the rate.
         Returns:
@@ -35,7 +117,7 @@ class Rate(abc.ABC):
         pass
     
     @abc.abstractmethod
-    def compute_rate_with_error(self):
+    def _compute_rate_with_error(self):
         """
         This method computes the rate with error.
         Returns:
@@ -77,16 +159,12 @@ class Rate(abc.ABC):
 
         plt.show()
     
-    def _compute_confidence_interval(self, function : Callable[[float]]) -> tuple[float, float]:
+    def _compute_confidence_interval(self) -> tuple[float, float]:
         """
         This method computes the confidence interval.
-        Args:
-            function (Callable[[float]]): The function to plot.
-            x (float): The x value.
-            error (float): The error of the rate.
         Returns:
             tuple[float, float]: The confidence interval.
         """
-        x_cont = np.linspace(self._xaxis.min(), self._xaxis.max(), endpoint=True)
-        y_function = function(x_cont)
+        x_cons = np.linspace(self._xaxis.min(), self._xaxis.max(), endpoint=True)
+        y_function = function(x_cons)
         pass
